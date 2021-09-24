3fcad384c6755ffcc85614da2093d16da7de8855
aearias23 <aarias231991@gmail.com>
2021-09-23 17:34:35 -0700

    I am starting the rock paper scissors project. My JS file has my pseudocode. Im currently able to select rock paper scissors randomly from an array.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,21 @@
+//I am making a rock paper scissors game.
+//In the HTML file I need to figure out how to have the user input the data.
+
+//Create an array with 'rock', 'paper', and 'scissors'.
+const weapons = ['Rock', 'Paper', 'Scissors'];
+//Have the function select rock, paper, scissors randomly
+computerPlay = function() {
+    return weapons[Math.floor(Math.random() * weapons.length)];
+}
+//Create a function to play a single game following two parameters-- 
+//playerSelection() and computerSelection()
+
+//and then return a string that declares the winner of the round like,
+//'You Lose! Paper beats Rock"
+// Make the function's playerSelection parameter case-insensitve.
+
+//Make a new function, game(). Put the previous function into this one to
+//play a 5 round game that keeps score and reports a winner or loser.
+//Perfect time to learn loops. You can also call playRound() function.
+
+//use prompt() to input data from user.
