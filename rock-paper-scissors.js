b135f6de61bbc2e8a2994c5bd7038e47c359c052
aearias23 <aarias231991@gmail.com>
2021-10-03 21:08:29 -0700

    I have figured out how to out put the IF statement into the HTML file. Also how to make the user input case insensitive.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,6 +5,10 @@ let computerScore = 0
 let roundWinner = ''
 //Create an array with 'rock', 'paper', and 'scissors'.
 const weapons = ['rock', 'paper', 'scissors'];
+// Make the function's playerSelection parameter case-insensitve.
+const weaponsCapitalized = ['ROCK', 'PAPER', 'SCISSORS'];
+weapons === weaponsCapitalized;
+weapons.toUpperCase() === weaponsCapitalized.toUpperCase();
 //Have the function select rock, paper, scissors randomly
 function computerPlay() {
     return weapons[Math.floor(Math.random() * weapons.length)];
@@ -12,7 +16,9 @@ function computerPlay() {
 //Create a function to play a single game following two parameters-- 
 //computerSelection and playerSelection 
 const computerSelection = computerPlay();
-const playerSelection = prompt();
+const playerSelection = prompt('Choose yoour weapon! Rock, paper, scissors.');
+
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection);
@@ -27,26 +33,38 @@ function playRound(playerSelection, computerSelection) {
         roundWinner = 'player';
     }
     if(
-        (playerSelection==='rock' && computerSelection==='paper') ||
-        (playerSelection==='paper' && computerSelection==='scissors') ||
-        (playerSelection==='scissors' && computerSelection==='rock')
+        (computerSelection==='rock' && playerSelection==='paper') ||
+        (computerSelection==='paper' && playerSelection==='scissors') ||
+        (computerSelection==='scissors' && playerSelection==='rock')
     );
     {
         roundWinner = 'computer';
     }
-
-if (roundWinner = 'player') {
-    greeting =  playerSelection + ' VS ' + computerSelection + '. You win!';
-}  else if (roundWinner = 'computer') {
-    greeting = playerSelection + ' VS ' + computerSelection + '. You lose!';
-} else {
-    greeting = playerSelection + ' VS ' + computerSelection + '. It\'s a tie!';
-}
-
-
 //and then return a string that declares the winner of the round like,
 //'You Lose! Paper beats Rock"
-// Make the function's playerSelection parameter case-insensitve.
+if (
+    (playerSelection==='rock' && computerSelection==='scissors') ||
+    (playerSelection==='paper' && computerSelection==='rock') ||
+    (playerSelection==='scissors' && computerSelection==='paper')
+) 
+{
+    document.getElementById("demo").innerHTML = playerSelection + ' VS ' 
+            + computerSelection + '. You win!';
+}  else if (
+    (computerSelection==='rock' && playerSelection==='paper') ||
+    (computerSelection==='paper' && playerSelection==='scissors') ||
+    (computerSelection==='scissors' && playerSelection==='rock')
+) {
+    document.getElementById("demo").innerHTML = playerSelection + ' VS '
+            + computerSelection + '. You lose!';
+} else if (playerSelection === computerSelection) {
+    document.getElementById("demo").innerHTML = playerSelection + ' VS ' 
+            + computerSelection + '. It\'s a tie!';
+};
+
+
+
+
 //Make a new function, game(). Put the previous function into this one to
 //play a 5 round game that keeps score and reports a winner or loser.
 //Perfect time to learn loops. You can also call playRound() function.
