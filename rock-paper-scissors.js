0760f8782191112a4baa6d3503baba0905bf1b30
aearias23 <aarias231991@gmail.com>
2021-10-06 17:29:58 -0700

    I got the playRound() to work properly. Now to loop it and have a way to maintain score.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,7 +4,7 @@ let playerScore = 0
 let computerScore = 0
 let roundWinner = ''
 //Create an array with 'rock', 'paper', and 'scissors'.
-const weapons = ['rock', 'paper', 'scissors', 'ROCK', 'PAPER', 'SCISSORS'];
+const weapons = ['rock', 'paper', 'scissors'];
 // Make the function's playerSelection parameter case-insensitve.
 
 //Have the function select rock, paper, scissors randomly
@@ -13,56 +13,52 @@ function computerPlay() {
 }
 //Create a function to play a single game following two parameters-- 
 //computerSelection and playerSelection 
-const computerSelection = computerPlay();
-const playerSelection = prompt('Choose yoour weapon! Rock, paper, scissors.');
+let computerSelection = computerPlay();
+let playerSelection = prompt('Choose your weapon! Rock, paper, scissors.');
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection);
-    roundWinner = 'tie';
+    if (playerSelection === computerSelection) {
+        
+        roundWinner = 'It\'s a tie!'
 }
     if (
         (playerSelection==='rock' && computerSelection==='scissors') ||
         (playerSelection==='paper' && computerSelection==='rock') ||
         (playerSelection==='scissors' && computerSelection==='paper') 
-    );
+    )
     {
-        roundWinner = 'player';
+        roundWinner = 'You win!'
+        playerScore++
     }
-    if(
-        (computerSelection==='rock' && playerSelection==='paper') ||
-        (computerSelection==='paper' && playerSelection==='scissors') ||
-        (computerSelection==='scissors' && playerSelection==='rock')
-    );
+    if (
+        (playerSelection==='rock' && computerSelection==='paper') ||
+        (playerSelection==='paper' && computerSelection==='scissors') ||
+        (playerSelection==='scissors' && computerSelection==='rock') 
+    )
     {
-        roundWinner = 'computer';
+        roundWinner = 'You lose!'
+        computerScore++
     }
 //and then return a string that declares the winner of the round like,
 //'You Lose! Paper beats Rock"
-if (
-    (playerSelection==='rock' && computerSelection==='scissors') ||
-    (playerSelection==='paper' && computerSelection==='rock') ||
-    (playerSelection==='scissors' && computerSelection==='paper')
-) 
-{
 //In the HTML file I need to figure out how to have the user input the data.
-    document.getElementById("demo").innerHTML = playerSelection + ' VS ' 
-            + computerSelection + '. You win!';
-}  else if (
-    (computerSelection==='rock' && playerSelection==='paper') ||
-    (computerSelection==='paper' && playerSelection==='scissors') ||
-    (computerSelection==='scissors' && playerSelection==='rock')
-) {
-    document.getElementById("demo").innerHTML = playerSelection + ' VS '
-            + computerSelection + '. You lose!';
-} else if (playerSelection === computerSelection) {
-    document.getElementById("demo").innerHTML = playerSelection + ' VS ' 
-            + computerSelection + '. It\'s a tie!';
-};
-
-
-
-
+     document.write(playerSelection + ' vs. ' + computerSelection + '. ' + roundWinner);
+}
 //Make a new function, game(). Put the previous function into this one to
 //play a 5 round game that keeps score and reports a winner or loser.
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    playRound(playerSelection, computerSelection);
+    for (let i = 0; i === playerScore === 5 || computerScore === 5; i++) {
+        if (playerScore === 5) {
+            greeting('You have won five rounds. Return home, you have' 
+                    + ' survived this battle...warrior')
+        } if (computerScore === 5) {
+            greeting('You have lost five rounds. You may rest now, warrior.' 
+                    + ' Thank you for your sacrifice')
+        }
+    } 
+}
 //Perfect time to learn loops. You can also call playRound() function.
 //use prompt() to input data from user.
